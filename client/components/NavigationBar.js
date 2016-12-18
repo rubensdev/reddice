9afa28861d6945e2de18ad73403539447554b1a9
Rubens García <rubensdeveloper@gmail.com>
2016-12-18 16:59:34 +0000

    Login and logout on client

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,20 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { logout } from '../actions/authActions';
 
-export default () => {
-	return(
-		<nav className="navbar navbar-default">
-			<div className="container-fluid">
-				<div className="navbar-header">
-					<Link to="/" className="navbar-brand">Red Dice</Link>
+class NavigationBar extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.logout = this.logout.bind(this);
+	}
+
+	logout(e) {
+		e.preventDefault();
+		this.props.logout();
+	}
+
+	render() {
+		const { isAuthenticated } = this.props.auth;
+
+		const userLinks = (
+			<ul className="nav navbar-nav navbar-right">
+				<li><a href="#" onClick={this.logout}>Logout</a></li>
+			</ul>
+		);
+
+		const guestLinks = (
+			<ul className="nav navbar-nav navbar-right">
+				<li><Link to="/login">Login</Link></li>
+				<li><Link to="/signup">Signup</Link></li>
+			</ul>
+		);
+
+		return(
+			<nav className="navbar navbar-default">
+				<div className="container-fluid">
+					<div className="navbar-header">
+						<Link to="/" className="navbar-brand">Red Dice</Link>
+					</div>
+					<div className="collapse navbar-collapse">
+						{ isAuthenticated ? userLinks : guestLinks }
+					</div>
 				</div>
-				<div className="collapse navbar-collapse">
-					<ul className="nav navbar-nav navbar-right">
-						<li><Link to="/login">Login</Link></li>
-						<li><Link to="/signup">Signup</Link></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+			</nav>
+		);
+	}
 }
+
+NavigationBar.PropTypes = {
+	auth: React.PropTypes.object.isRequired,
+	logout: React.PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+	return {
+		auth: state.auth
+	}
+}
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
